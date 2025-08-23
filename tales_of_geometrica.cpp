f9d1f991d391ea7cbcd2a3d89e28409261354760
longevity-software <longevity_software@mail.com>
2025-08-23 13:35:51 +0100

    Adding Sprite which is rendered to the screen at mouse position currently.

--- tales_of_geometrica.cpp
+++ tales_of_geometrica.cpp
@@ -4,6 +4,10 @@
 class TalesOfGeometrica : public olc::PixelGameEngine
 {
 public:
+
+    olc::Renderable _mathildaDialogueSprite;
+
+public:
 	TalesOfGeometrica()
 	{
 		sAppName = "Tales of Geometrica";
@@ -13,24 +17,25 @@ public:
 	bool OnUserCreate() override
 	{
 		// Called once at the start, so create things here
+        _mathildaDialogueSprite.Load("assets/gfx/MathildaBaseDialogue.png");
+
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		// called once per frame
-		for (int x = 0; x < ScreenWidth(); x++)
-			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand()% 256));	
+        Clear(olc::BLACK);
+        
+        DrawDecal(GetMousePos(), _mathildaDialogueSprite.Decal());
+
 		return true;
 	}
 };
 
-
 int main()
 {
 	TalesOfGeometrica game;
-	if (game.Construct(256, 240, 4, 4))
+	if (game.Construct(240, 160, 1, 1))
 		game.Start();
 
 	return 0;
